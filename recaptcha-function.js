bd520ef1e592275e24de5d99d404704dd9ebbebc
Muse You Could Use <museyoucoulduse@gmail.com>
2018-10-23 02:58:23 +0200

    Fixed

--- recaptcha-function.js
+++ recaptcha-function.js
@@ -1,14 +1,15 @@
 const rp = require('request-promise');
-const recaptcha = require('./secrets/google').recaptcha;
+const recaptcha = require('./secrets/google').recaptcha_localhost;
 
 module.exports.reCAPTCHA = (req, res)  => {
   url = `https://www.google.com/recaptcha/api/siteverify?secret=${recaptcha}&response=${req.body['g-recaptcha-response']}`
   return rp({
     url: url,
+    method: 'POST',
     json: true,
-    method: 'POST'
   })
   .then(response => {
+    console.log(response)
     if (response.success) {
       return res.send({
         success: true
